4fe99c2236a974a9ff293266eba4377b4136164a
Xavier <xavier194152@aol.com>
2019-04-14 03:33:55 -0700

    Add files via upload

---
+++ Mouse_interrupter.ino
@@ -0,0 +1,54 @@
+#include <Mouse.h>
+void setup() {
+  // put your setup code here, to run once:
+    Mouse.begin();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  delay(5000);
+  //move left
+  Mouse.move(10, 0);
+  delay(100);
+  Mouse.move(10, 0);
+  delay(100);
+  Mouse.move(10, 0);
+  delay(100);
+  Mouse.move(10, 0);
+  delay(100);
+  Mouse.move(10, 0);
+  delay(100);
+  //move right
+  Mouse.move(0, 10);
+  delay(100);
+  Mouse.move(0, 10);
+  delay(100);
+  Mouse.move(0, 10);
+  delay(100);
+  Mouse.move(0, 10);
+  delay(100);
+  Mouse.move(0, 10);
+  delay(100);
+  //move up
+  Mouse.move(-10, 0);
+  delay(100);
+  Mouse.move(-10, 0);
+  delay(100);
+  Mouse.move(-10, 0);
+  delay(100);
+  Mouse.move(-10, 0);
+  delay(100);
+  Mouse.move(-10, 0);
+  delay(100);
+  //move down
+  Mouse.move(0, -10);
+  delay(100);
+  Mouse.move(0, -10);
+  delay(100);
+  Mouse.move(0, -10);
+  delay(100);
+  Mouse.move(0, -10);
+  delay(100);
+  Mouse.move(0, -10);
+  delay(100);
+}
